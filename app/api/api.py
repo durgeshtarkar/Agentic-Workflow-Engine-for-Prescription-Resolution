2c4fde1c91ff09ac1a462c41efee12efdb22c46f
Aman Sorout <amansorout777@gmail.com>
2026-04-30 21:36:16 +0530

    Removed all files from aman branch

--- app/api/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.api.endpoints import auth, prescriptions, drugs
-
-api_router = APIRouter()
-
-api_router.include_router(auth.router, prefix="/auth", tags=["authentication"])
-api_router.include_router(prescriptions.router, prefix="/prescriptions", tags=["prescriptions"])
-api_router.include_router(drugs.router, prefix="/drugs", tags=["pharmacology"])
